349bc2680c3e568ee90690420b3e05deba416c02
agrizzli <arthur_mueller@gmx.net>
2019-11-15 21:04:11 +0100

    Add algorithm property to training order

--- src/adss/TrainingOrder.java
+++ src/adss/TrainingOrder.java
@@ -7,4 +7,30 @@ package adss;
  *  - And finally backwards
  */
 public class TrainingOrder extends Order {
+
+    /**
+     * Algorithm to use for training and also for prediction later.
+     * This information must:
+     *  - be set by UserAgent
+     *  - then transported to ManagerAgent, which creates ClassifierAgents for this algorithm
+     */
+    private String algorithm = "J48";
+
+    /**
+     * Set classification algorithm name
+     *
+     * @param algorithm Classification algorithm name from configuration file
+     */
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    /**
+     * Get classification algorithm name
+     *
+     * @return Classification algorithm name from configuration file
+     */
+    public String getAlgorithm() {
+        return algorithm;
+    }
 }
